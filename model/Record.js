df01129a3b85a0132f4e101005550d1e8d50334f
Whast <danikhpanik0707@gmail.com>
2024-06-29 23:07:31 +0400

    iduser

--- model/Record.js
+++ model/Record.js
@@ -13,9 +13,8 @@ const RecordSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
+    user: {
+        type: String,
         required:true
     }
 });
